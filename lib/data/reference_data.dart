e538656297be7276caa2220075b8f5f1938ddfa6
ffelixm <192540314+ffelixm@users.noreply.github.com>
2025-07-08 19:51:23 +0200

    Improve reference descriptions

--- lib/data/reference_data.dart
+++ lib/data/reference_data.dart
@@ -28,10 +28,10 @@ List<ReferenceSectionCardData> getReferenceSectionCardData(ReferenceSectionSubse
     ReferenceSectionCardData(
       "Year", FormatType.number, "y",
       [
-        ReferenceSectionCardRowData("y", "Year as number"),
+        ReferenceSectionCardRowData("y", "Year as a number"),
         ReferenceSectionCardRowData("yy", "Short year (only last 2 digits)"),
         ReferenceSectionCardRowData("yyyy", "Full year, displayed with 4 digits"),
-        ReferenceSectionCardRowData("yyyyy", "5 or more characters: full year padded with leading zeros"),
+        ReferenceSectionCardRowData("yyyyy", "5 or more characters: full year as a number with at least n digits, padded with leading zeros"),
       ],
     ),
   ],
@@ -39,12 +39,12 @@ List<ReferenceSectionCardData> getReferenceSectionCardData(ReferenceSectionSubse
     ReferenceSectionCardData(
       "Month", FormatType.textAndNumber, "M",
       [
-        ReferenceSectionCardRowData("M", "Month in year as number"),
-        ReferenceSectionCardRowData("MM", "Month in year as 2-digit number with leading zero"),
+        ReferenceSectionCardRowData("M", "Month in year as a number"),
+        ReferenceSectionCardRowData("MM", "Month in year as a 2-digit number with leading zero"),
         ReferenceSectionCardRowData("MMM", "Short form of month name (3 letters)"),
         ReferenceSectionCardRowData("MMMM", "Long name of month"),
         ReferenceSectionCardRowData("MMMMM", "First letter of month name"),
-        ReferenceSectionCardRowData("MMMMMM", "6 or more characters: month in year as number padded with leading zeros"),
+        ReferenceSectionCardRowData("MMMMMM", "6 or more characters: month in year with at least n digits, padded with leading zeros"),
       ],
     ),
     ReferenceSectionCardData(
@@ -60,8 +60,8 @@ List<ReferenceSectionCardData> getReferenceSectionCardData(ReferenceSectionSubse
     ReferenceSectionCardData(
       "Day", FormatType.number, "d",
       [
-        ReferenceSectionCardRowData("d", "Day in month as number"),
-        ReferenceSectionCardRowData("dd", "2 or more characters: day in month as number with at least n digits, paddded with leading zeros if necessary"),
+        ReferenceSectionCardRowData("d", "Day in month as a number"),
+        ReferenceSectionCardRowData("dd", "2 or more characters: day in month as a number with at least n digits, padded with leading zeros if necessary"),
       ],
     ),
     ReferenceSectionCardData(
@@ -75,8 +75,8 @@ List<ReferenceSectionCardData> getReferenceSectionCardData(ReferenceSectionSubse
     ReferenceSectionCardData(
       "Weekday", FormatType.text, "E",
       [
-        ReferenceSectionCardRowData("E", "Short form of EEE; displays short weekday name (3 letters)"),
-        ReferenceSectionCardRowData("EEE", "Short and abbreviated weekday name"),
+        ReferenceSectionCardRowData("E", "Short form of EEE; displays short weekday name"),
+        ReferenceSectionCardRowData("EEE", "Short weekday name"),
         ReferenceSectionCardRowData("EEEE", "Long weekday name"),
         ReferenceSectionCardRowData("EEEEE", "First letter of weekday name"),
       ],
@@ -84,8 +84,8 @@ List<ReferenceSectionCardData> getReferenceSectionCardData(ReferenceSectionSubse
     ReferenceSectionCardData(
       "Day in year", FormatType.text, "D",
       [
-        ReferenceSectionCardRowData("D", "Day in year as number"),
-        ReferenceSectionCardRowData("DDDD", "2 or more characters: day in year as number with at least n digits, paddded with leading zeros if necessary"),
+        ReferenceSectionCardRowData("D", "Day in year as a number"),
+        ReferenceSectionCardRowData("DDDD", "2 or more characters: day in year as a number with at least n digits, padded with leading zeros if necessary"),
       ],
     ),
   ],
@@ -93,19 +93,19 @@ List<ReferenceSectionCardData> getReferenceSectionCardData(ReferenceSectionSubse
     ReferenceSectionCardData(
       "Hour in am/pm", FormatType.number, "h/K",
       [
-        ReferenceSectionCardRowData("h", "Hour in am/pm as number (1-12)"),
-        ReferenceSectionCardRowData("hh", "2 or more characters: hour in am/pm (1-12) as number with at least n digits, paddded with leading zeros if necessary"),
-        ReferenceSectionCardRowData("K", "Hour in am/pm as number (0-11)"),
-        ReferenceSectionCardRowData("KK", "2 or more characters: hour in am/pm (0-11) as number with at least n digits, paddded with leading zeros if necessary"),
+        ReferenceSectionCardRowData("h", "Hour in am/pm as a number (1-12)"),
+        ReferenceSectionCardRowData("hh", "2 or more characters: hour in am/pm (1-12) as a number with at least n digits, padded with leading zeros if necessary"),
+        ReferenceSectionCardRowData("K", "Hour in am/pm as a number (0-11)"),
+        ReferenceSectionCardRowData("KK", "2 or more characters: hour in am/pm (0-11) as a number with at least n digits, padded with leading zeros if necessary"),
       ],
     ),
     ReferenceSectionCardData(
       "Hour in day", FormatType.number, "H/k",
       [
-        ReferenceSectionCardRowData("H", "Hour in day as number (0-23)"),
-        ReferenceSectionCardRowData("HH", "2 or more characters: hour in day (0-23) as number with at least n digits, paddded with leading zeros if necessary"),
-        ReferenceSectionCardRowData("k", "Hour in day as number (1-24)"),
-        ReferenceSectionCardRowData("kk", "2 or more characters: hour in day (1-24) as number with at least n digits, paddded with leading zeros if necessary"),
+        ReferenceSectionCardRowData("H", "Hour in day as a number (0-23)"),
+        ReferenceSectionCardRowData("HH", "2 or more characters: hour in day (0-23) as a number with at least n digits, padded with leading zeros if necessary"),
+        ReferenceSectionCardRowData("k", "Hour in day as a number (1-24)"),
+        ReferenceSectionCardRowData("kk", "2 or more characters: hour in day (1-24) as a number with at least n digits, padded with leading zeros if necessary"),
       ],
     ),
   ],
@@ -113,8 +113,8 @@ List<ReferenceSectionCardData> getReferenceSectionCardData(ReferenceSectionSubse
     ReferenceSectionCardData(
       "Minute", FormatType.number, "m",
       [
-        ReferenceSectionCardRowData("m", "Minute in hour as number"),
-        ReferenceSectionCardRowData("mm", "2 or more characters: minute in hour as number with at least n digits, paddded with leading zeros if necessary"),
+        ReferenceSectionCardRowData("m", "Minute in hour as a number"),
+        ReferenceSectionCardRowData("mm", "2 or more characters: minute in hour as a number with at least n digits, padded with leading zeros if necessary"),
       ],
     ),
   ],
@@ -122,16 +122,16 @@ List<ReferenceSectionCardData> getReferenceSectionCardData(ReferenceSectionSubse
     ReferenceSectionCardData(
       "Second", FormatType.number, "s",
       [
-        ReferenceSectionCardRowData("s", "Second in minute as number"),
-        ReferenceSectionCardRowData("ss", "2 or more characters: second in minute as number with at least n digits, paddded with leading zeros if necessary"),
+        ReferenceSectionCardRowData("s", "Second in minute as a number"),
+        ReferenceSectionCardRowData("ss", "2 or more characters: second in minute as a number with at least n digits, padded with leading zeros if necessary"),
       ],
     ),
     ReferenceSectionCardData(
       "Fractional second", FormatType.number, "S",
       [
-        ReferenceSectionCardRowData("S", "Short form of SSS; displays fractional seconds as 3-digit number"),
-        ReferenceSectionCardRowData("SSS", "Fractional second as 3-digit number"),
-        ReferenceSectionCardRowData("SSSS", "4 or more characters: fractional second as 3-digit number with trailing zeros"),
+        ReferenceSectionCardRowData("S", "Short form of SSS; displays fractional seconds as a 3-digit number"),
+        ReferenceSectionCardRowData("SSS", "Fractional second as a 3-digit number"),
+        ReferenceSectionCardRowData("SSSS", "4 or more characters: fractional second as a 3-digit number with trailing zeros"),
       ],
     ),
   ],
@@ -152,11 +152,11 @@ List<ReferenceSectionCardData> getReferenceSectionCardData(ReferenceSectionSubse
     ReferenceSectionCardData(
       "Quarter", FormatType.textAndNumber, "Q",
       [
-        ReferenceSectionCardRowData("Q", "Quarter as number"),
-        ReferenceSectionCardRowData("QQ", "Quarter as 2-digit number with leading zero"),
+        ReferenceSectionCardRowData("Q", "Quarter as a number"),
+        ReferenceSectionCardRowData("QQ", "Quarter as a 2-digit number with leading zero"),
         ReferenceSectionCardRowData("QQQ", "Short form of quarter (Q1-Q4)"),
         ReferenceSectionCardRowData("QQQQ", "Full name of quarter"),
-        ReferenceSectionCardRowData("QQQQQ", "5 or more characters: quarter as number padded with leading zeros"),
+        ReferenceSectionCardRowData("QQQQQ", "5 or more characters: quarter as a number with at least n digits, padded with leading zeros"),
       ],
     ),
     ReferenceSectionCardData(
