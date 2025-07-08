dfb41112ceb612a9b874ccb1f7dbbd415cdb481c
ffelixm <192540314+ffelixm@users.noreply.github.com>
2025-07-08 12:45:43 +0200

    Initialize all locales for date formatting on app start

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:date_format_playground/services/theme_service.dart';
 import 'package:date_format_playground/ui/home_view.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  await initializeDateFormatting();
   final ThemeMode themeMode = await loadThemeMode();
 
   runApp(App(themeMode: themeMode));
