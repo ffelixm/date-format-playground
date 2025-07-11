a149c130bac313fd1a005af438995208144e9ead
ffelixm <192540314+ffelixm@users.noreply.github.com>
2025-07-11 14:17:38 +0200

    Add small help FAB that shows dialog with help and information on how to use this website

--- lib/ui/home_view.dart
+++ lib/ui/home_view.dart
@@ -3,6 +3,7 @@ import 'package:date_format_playground/data/section_models.dart';
 import 'package:date_format_playground/ui/dateformat_update_ticker.dart';
 import 'package:date_format_playground/ui/home/playground_card.dart';
 import 'package:date_format_playground/ui/home/reference_section.dart';
+import 'package:date_format_playground/ui/widgets/html_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart' show dateTimeSymbolMap;
 
@@ -114,6 +115,38 @@ class _HomeViewState extends State<HomeView> {
             ),
           );
         },
+      ),
+      floatingActionButton: FloatingActionButton.small(
+        onPressed: () => showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: Text("Help"),
+            content: Container(
+              constraints: BoxConstraints(maxWidth: 700),
+              child: HtmlWidget(html: """
+This webseite is intended to be a playground for date formatting in Dart/Flutter using the <a href="https://pub.dev/packages/intl">intl package</a>.
+<br><br>
+On top you can see the playground. Input any format pattern and see the result immediately.
+<br>
+In the top right corner you can toggle light/dark mode, set the locale for all date formatting and play/pause periodic format updates.
+<br><br>
+The rest of this site is divided into 4 sections:
+<ul>
+<li><b>Cheatsheet</b>: List of all available format symbols</li>
+<li><b>Skeletons</b>: List of all available skeletons. Skeletons are presets with an own constructor and returns real localized results</li>
+<li><b>Reference</b>: Full reference of all usage cases of all format symbols</li>
+<li><b>About</b>: About this app</li>
+</ul>"""),
+            ),
+            actions: [
+              TextButton(
+                onPressed: () => Navigator.pop(context),
+                child: Text("Close"),
+              )
+            ],
+          ),
+        ),
+        child: Icon(Icons.question_mark),
       ),
     );
   }
